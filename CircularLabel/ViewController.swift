089970d01b861937f5e2b58cece5b71111c63d33
iDoyoung <ido.young@icloud.com>
2021-11-04 23:03:51 +0900

    Fix theta

--- CircularLabel/ViewController.swift
+++ CircularLabel/ViewController.swift
@@ -27,30 +27,31 @@ class ViewController: UIViewController {
 }
 
 class ArcLabelView: UIView {
+    
+    let allText = ["Test1", "Test2", "Test3", "Test4", "Test5", "Test6", "Test7", "Test8"]
     override func draw(_ rect: CGRect) {
         guard let context = UIGraphicsGetCurrentContext() else { return }
-        let size = self.bounds.size
-
-        context.translateBy (x: size.width / 2, y: size.height / 2)
+        context.translateBy(x: self.bounds.midX, y: self.bounds.midX)
         context.scaleBy (x: 1, y: -1)
 
-        centreArcPerpendicular(text: "Hello world. Hello Korea. Hello USA. Hello Canada. Hello UK. Hello Brazil. Hello Finland. Hello Sweden. Hello Japan", context: context, radius: 100, angle: 0, colour: UIColor.red, font: UIFont.systemFont(ofSize: 16), clockwise: true)
+        centreArcPerpendicular(text: allText, context: context, radius: (self.bounds.width - 50) / 2, angle: 0, colour: UIColor.red, font: UIFont.systemFont(ofSize: 50), clockwise: true)
     }
     
-    func centreArcPerpendicular(text str: String, context: CGContext, radius r: CGFloat, angle theta: CGFloat, colour c: UIColor, font: UIFont, clockwise: Bool){
+    func centreArcPerpendicular(text str: [String], context: CGContext, radius r: CGFloat, angle theta: CGFloat, colour c: UIColor, font: UIFont, clockwise: Bool){
         
-        let characters: [String] = str.map { String($0) } // An array of single character strings, each character in str
+        let characters: [[Character]] = str.map { Array($0) } // An array of single character strings, each character in str
         let l = characters.count
         let attributes = [NSAttributedString.Key.font: font]
 
-        var arcs: [CGFloat] = [] // This will be the arcs subtended by each character
-        var totalArc: CGFloat = 0 // ... and the total arc subtended by the string
+        //var arcs: [CGFloat] = [] // This will be the arcs subtended by each character
+        let totalArc: CGFloat = 360 // ... and the total arc subtended by the string
 
         // Calculate the arc subtended by each letter and their total
-        for i in 0 ..< l {
-            arcs += [chordToArc(characters[i].size(withAttributes: attributes).width, radius: r)]
-            totalArc += arcs[i]
-        }
+//        for i in 0 ..< l {
+//            //MARK: chord 길이 고정하기
+//            arcs += [chordToArc(characters[i].size(withAttributes: attributes).width, radius: r)]
+//            totalArc += arcs[i]
+//        }
 
         // Are we writing clockwise (right way up at 12 o'clock, upside down at 6 o'clock)
         // or anti-clockwise (right way up at 6 o'clock)?
@@ -63,18 +64,26 @@ class ArcLabelView: UIView {
         var thetaI = theta - direction * totalArc / 2
 
         for i in 0 ..< l {
-            thetaI += direction * arcs[i] / 2
+            thetaI += direction * (.pi * self.frame.width / 8) / 2
             // Call centerText with each character in turn.
             // Remember to add +/-90º to the slantAngle otherwise
             // the characters will "stack" round the arc rather than "text flow"
-            centre(text: characters[i], context: context, radius: r, angle: thetaI, colour: c, font: font, slantAngle: thetaI + slantCorrection)
+            var newThetaI = thetaI
+            for index in characters[i].indices {
+                let char = String(characters[i][index])
+                newThetaI += direction * (chordToArc(char.size(withAttributes: attributes).width, radius: r)) / 2
+                centre(text: char, context: context, radius: r, angle: newThetaI, colour: c, font: font, slantAngle: newThetaI + slantCorrection)
+                newThetaI += direction * (chordToArc(char.size(withAttributes: attributes).width, radius: r)) / 2
+            }
+//            centre(text: characters[i], context: context, radius: r, angle: thetaI, colour: c, font: font, slantAngle: thetaI + slantCorrection)
             // The centre of the next character will then be at
             // thetaI = thetaI + arcs[i] / 2 + arcs[i + 1] / 2
             // but again we leave the last term to the start of the next loop...
-            thetaI += direction * arcs[i] / 2
+            thetaI += direction * (.pi * self.frame.width / 8) / 2
         }
     }
 
+    //chord = 현
     func chordToArc(_ chord: CGFloat, radius: CGFloat) -> CGFloat {
         return 2 * asin(chord / (2 * radius))
     }
